e2645b59658332dc4e4b5b8392d0f38240bb2146
Aditya Bhardwaj <adityabhardwaj13579@gmail.com>
2025-04-24 23:11:38 +0530

    FeedbackRepository.java build done

--- College-event-management/src/main/java/com/collegeevent/KajalK11/College_event_management/repository/FeedbackRepository.java
+++ College-event-management/src/main/java/com/collegeevent/KajalK11/College_event_management/repository/FeedbackRepository.java
@@ -4,10 +4,17 @@ import com.collegeevent.KajalK11.College_event_management.model.Feedback;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+public class FeedbackRepository { package com.collegeevent.KajalK11.College_event_management.repository;
+
+import com.collegeevent.KajalK11.College_event_management.model.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
-@Repository
-public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
-    List<Feedback> findByEventId(Long eventId);
-    List<Feedback> findByUserId(Long userId);
+    @Repository
+    public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+        List<Feedback> findByEventId(Long eventId);
+        List<Feedback> findByUserId(Long userId);
+    }
 }
